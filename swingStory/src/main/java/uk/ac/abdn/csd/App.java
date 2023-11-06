9b67a79b2b6e8a018df438f379da2044d8b4de17
F-gigabyte <ftlg987@gmail.com>
2023-11-06 14:23:54 +0000

    Initialised with maven

---
+++ swingStory/src/main/java/uk/ac/abdn/csd/App.java
@@ -0,0 +1,13 @@
+package uk.ac.abdn.csd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
