406d18606e77aa07830f76dbdac162b298d44850
F-gigabyte <ftlg987@gmail.com>
2023-11-06 15:02:29 +0000

    First GUI

--- swingStory/src/main/java/uk/ac/abdn/csd/App.java
+++ swingStory/src/main/java/uk/ac/abdn/csd/App.java
@@ -1,6 +1,8 @@
 package uk.ac.abdn.csd;
 
 import com.github.javafaker.*;
+import java.awt.BorderLayout;
+import javax.swing.*;
 
 /**
  * Hello world!
@@ -8,12 +10,41 @@ import com.github.javafaker.*;
  */
 public class App 
 {
-    public static void main( String[] args )
+    private static String genStory()
     {
         Faker faker = new Faker();
         String name = faker.name().fullName();
         String firstName = faker.name().firstName();
         String lastName = faker.name().lastName();
-        System.out.printf("Hello %s and %s %s\n", name, firstName, lastName);
+        String story = "Hello " + name + " and " + lastName + ", " + firstName;
+        return story;
+    }
+
+    private static void createAndShowGUI()
+    {
+        // Window
+        JFrame frame = new JFrame("Swing Story Generator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // getting story
+        String story = genStory();
+
+        // Labels
+        JLabel introLabel = new JLabel("The story goes: ");
+        JLabel storyLabel = new JLabel(story);
+        frame.getContentPane().add(introLabel, BorderLayout.PAGE_START);
+        frame.getContentPane().add(storyLabel, BorderLayout.LINE_START);
+        frame.setSize(400, 300);
+        frame.setVisible(true);
+    }
+
+    public static void main( String[] args )
+    {
+        javax.swing.SwingUtilities.invokeLater(new Runnable()
+                {
+                    public void run()
+                    {
+                        createAndShowGUI();
+                    }
+                });
     }
 }
